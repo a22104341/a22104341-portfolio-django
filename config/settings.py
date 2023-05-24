37f304b500ad6d32a1a8cec9925801c3c6011447
a22104341 <diogo.m.rodrigues2003@googlemail.com>
2023-05-24 10:42:03 +0100

    fisrt commitfisrt commit

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# config/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('portfolio.urls')),
+]
 
 # Application definition
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,12 +6,7 @@ app_name = 'portfolio'
 
 urlpatterns = [
     path('', views.index_view),
-    path('home', views.home_page_view, name='home'),
     path('index', views.index_view),
-    path('products', views.products_view, name='products'),
-    path('about', views.about_view, name='about'),
-    path('contact', views.contact_view, name='contact'),
 ]
-#  hello/urls.py
-
+#hello/urls.py
 from django.shortcuts import render
