a3468bd65fc5da262ec6db0a71c38492416f5b7d
a22104341 <diogo.m.rodrigues2003@googlemail.com>
2023-05-24 11:58:18 +0100

    YourMum

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    firstName = models.CharField(max_length=250, null = False)
+    lastName = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null = False)
+    phoneNr = models.IntegerField(null = False)
+    birthdate = models.DateField()
+    verified = models.BooleanField(null = False)
+
+    def __str__(self):
+        return self.email
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, null = False)
+    content = models.CharField(max_length=1000, null = False)
+    date = models.DateField(null = False)
+    #image = models.ImageField()
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name = "posts")
+
+    def __str__(self):
+        return self.title
