e5638d77e7d74dc554b1f770f97797042e2943f8
a22104341 <diogo.m.rodrigues2003@googlemail.com>
2023-05-31 11:50:53 +0100

    YourMum

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Author, Post
+from .models import Author, BlogOwner, Blog, Area, Article, Comment, Like
 
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(BlogOwner)
+admin.site.register(Blog)
+admin.site.register(Area)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Like)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Author, BlogOwner, Blog, Area, Article, Comment, Like
 # Create your views here.
 #  hello/views.py
 
 from django.shortcuts import render
 
+
 def home_page_view(request):
-	return render(request, 'portfolio/home.html')
+    return render(request, 'portfolio/home.html')
