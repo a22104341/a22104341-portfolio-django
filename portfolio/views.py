653168ab6aadd5c21e9ab91fade6549da273d835
a22104341 <diogo.m.rodrigues2003@googlemail.com>
2023-06-07 12:11:56 +0100

    yes

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
-from .models import Author, BlogOwner, Blog, Area, Article, Comment, Like
 # Create your views here.
 #  hello/views.py
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AuthorForm
 
 
 def home_page_view(request):
     return render(request, 'portfolio/home.html')
+
+
+def blog_page_view(request):
+    return render(request, 'portfolio/blog.html')
+
+
+def author_register_page_view(request):
+    form = AuthorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('tarefas:home')
+    context = {'form': form}
+    return render(request, 'portfolio/register-author.html', context)
